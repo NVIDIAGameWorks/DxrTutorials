1363bdf9f37994657bc20f9f3399c00a0bfd07c0
nbenty <nbenty@nvidia.com>
2018-04-02 21:43:05 -0700

    Insert a UAV barrier after writing to the BLAS

--- Tutorials/07-BasicShaders/07-BasicShaders.cpp
+++ Tutorials/07-BasicShaders/07-BasicShaders.cpp
@@ -348,6 +348,12 @@ AccelerationStructureBuffers createBottomLevelAS(ID3D12DevicePtr pDevice, ID3D12
     ID3D12CommandListRaytracingPrototypePtr pRtCmdList = pCmdList;
     pRtCmdList->BuildRaytracingAccelerationStructure(&asDesc);
 
+    // We need to insert a UAV barrier before using the acceleration structures in a raytracing operation
+    D3D12_RESOURCE_BARRIER uavBarrier = {};
+    uavBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV;
+    uavBarrier.UAV.pResource = buffers.pResult;
+    pCmdList->ResourceBarrier(1, &uavBarrier);
+
     return buffers;
 }
 
--- Tutorials/14-Refit/14-Refit.cpp
+++ Tutorials/14-Refit/14-Refit.cpp
@@ -362,6 +362,12 @@ DxrSample::AccelerationStructureBuffers createBottomLevelAS(ID3D12DevicePtr pDev
     ID3D12CommandListRaytracingPrototypePtr pRtCmdList = pCmdList;
     pRtCmdList->BuildRaytracingAccelerationStructure(&asDesc);
 
+    // We need to insert a UAV barrier before using the acceleration structures in a raytracing operation
+    D3D12_RESOURCE_BARRIER uavBarrier = {};
+    uavBarrier.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV;
+    uavBarrier.UAV.pResource = buffers.pResult;
+    pCmdList->ResourceBarrier(1, &uavBarrier);
+
     return buffers;
 }
 
